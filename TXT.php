f67bcc56c8c5f3d93d3d6723146489c16bed27cd
velayuthampr <velayutham@arivuventures.com>
2015-03-05 17:30:57 +0530

    Converted the pho to rich text document
    this feature is added by mistake. But this looks cool.

---
+++ TXT.php
@@ -0,0 +1,76 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1344\cocoasubrtf720
+{\fonttbl\f0\froman\fcharset0 Times-Roman;\f1\fnil\fcharset0 HelveticaNeue;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;\red179\green179\blue179;\red0\green0\blue233;
+\red255\green255\blue255;}
+{\*\listtable{\list\listtemplateid1\listhybrid{\listlevel\levelnfc23\levelnfcn23\leveljc0\leveljcn0\levelfollow0\levelstartat1\levelspace360\levelindent0{\*\levelmarker \{disc\}}{\leveltext\leveltemplateid1\'01\uc0\u8226 ;}{\levelnumbers;}\fi-360\li720\lin720 }{\listname ;}\listid1}}
+{\*\listoverridetable{\listoverride\listid1\listoverridecount0\ls1}}
+{\info
+{\title My Race India}}\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720
+
+\f0\fs24 \cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 load->helper('url') ?>\
+\pard\pardeftab720
+
+\f1\fs22 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 Toggle navigation {\field{\*\fldinst{HYPERLINK "file:///Users/ArivuMacPro/Documents/Feedback%20app/%3C?php%20echo%20site_url()%20?%3E"}}{\fldrslt 
+\f0\fs24 \cf4 \cb1 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 \expnd0\expndtw0\kerning0
+\ul \outl0\strokewidth0  }}
+\f0\fs24 \cb1 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \
+\pard\tx220\tx720\pardeftab720\li720\fi-720
+\ls1\ilvl0\cf4 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	\'95	}{\field{\*\fldinst{HYPERLINK "file:///Users/ArivuMacPro/Documents/Feedback%20app/%3C?php%20echo%20site_url()%20?%3E"}}{\fldrslt \expnd0\expndtw0\kerning0
+\ul \outl0\strokewidth0 \strokec4 Home}}\cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2  \
+\ls1\ilvl0\cf4 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	\'95	}{\field{\*\fldinst{HYPERLINK "file:///Users/ArivuMacPro/Documents/Feedback%20app/%3C?php%20echo%20site_url()%20?%3E/#scroll-about"}}{\fldrslt \expnd0\expndtw0\kerning0
+\ul \outl0\strokewidth0 \strokec4 About Us}}\cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2  \
+\ls1\ilvl0\cf4 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	\'95	}{\field{\*\fldinst{HYPERLINK "file:///Users/ArivuMacPro/Documents/Feedback%20app/%3C?php%20echo%20site_url()%20?%3E/#scroll-services"}}{\fldrslt \expnd0\expndtw0\kerning0
+\ul \outl0\strokewidth0 \strokec4 Services}}\cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2  \
+\ls1\ilvl0\cf4 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	\'95	}{\field{\*\fldinst{HYPERLINK "file:///Users/ArivuMacPro/Documents/Feedback%20app/%3C?php%20echo%20site_url()%20?%3E/#scroll-gallery"}}{\fldrslt \expnd0\expndtw0\kerning0
+\ul \outl0\strokewidth0 \strokec4 MyRace Gallery}}\cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2  \
+\ls1\ilvl0\cf4 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	\'95	}{\field{\*\fldinst{HYPERLINK "file:///Users/ArivuMacPro/Documents/Feedback%20app/%3C?php%20echo%20site_url()%20?%3E/#scroll-footer"}}{\fldrslt \expnd0\expndtw0\kerning0
+\ul \outl0\strokewidth0 \strokec4 Testimonials}}\cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2  \
+\ls1\ilvl0\cf4 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	\'95	}{\field{\*\fldinst{HYPERLINK "file:///Users/ArivuMacPro/Documents/Feedback%20app/%3C?php%20echo%20site_url()%20?%3E/home/results"}}{\fldrslt \expnd0\expndtw0\kerning0
+\ul \outl0\strokewidth0 \strokec4 Race Results}}\cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2  
+\f1\fs22 \cb5 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \
+\pard\pardeftab720
+\cf2                         2012; $i--)\{ ?>\
+                        \
+                        \
+                    \
+                        
+\f0 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 Select Event Name
+\f1 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \
+                        
+\f0 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 Tanjore Marathon 2015
+\f1 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \
+                        
+\f0 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 Cool Runners Republic Day Half Marathon 2015
+\f1 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \
+                    
+\f0\fs24 \cb1 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0  
+\f1\fs22 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 Get My Race Results
+\f0\fs24 \cb1 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0  \
+\
+\pard\pardeftab720\sa320
+
+\fs32 \cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 Please Enter Event Name to Search \
+}
